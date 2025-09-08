bd8a08f1df916023e6009e698f22b714b1302890
SagunTyagi <shagunt150@gmail.com>
2025-09-08 11:42:00 +0530

    fully updated

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-import { Plus, Download, Search, Edit, Trash2, X } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import React, { useState, useEffect } from "react";
