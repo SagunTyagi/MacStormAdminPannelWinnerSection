abb00c4172c86078f952c043944daf135f43ba0c
SagunTyagi <146172785+SagunTyagi@users.noreply.github.com>
2025-10-15 17:57:59 +0530

    Update AllTrasactions.jsx
    .

--- src/pages/AllTrasactions.jsx
+++ src/pages/AllTrasactions.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from  'react';
 import { Search, ChevronLeft, ChevronRight } from 'lucide-react';
 
 const TransactionsTable = () => {
